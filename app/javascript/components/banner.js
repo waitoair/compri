c448221df2f51d5f6463ca51c8a15326941ee767
lara barbosa <larapattylprb@gmail.com>
2021-06-16 12:22:33 -0300

    bug parcialmente resolvido

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,9 +1,11 @@
 import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
-  new Typed('#titulo', {
-    strings: ["Your favorite book at the best stores at the lowest price."],
+  if (document.getElementById("titulo")) {
+    new Typed('#titulo', {
+    strings: ["Your favorite book at the best stores at the lowest price."], 
     typeSpeed: 45,
     loop: true
-  });
+    });
+  }
 }
 export { loadDynamicBannerText };
